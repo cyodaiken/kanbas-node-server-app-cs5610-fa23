03d6b0156fbe2fefe943e9f88960aeb823347c4c
Chaia <chaia.yodaiken@gmail.com>
2023-11-14 20:40:26 -0500

    adding config

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from "cors";
+import "dotenv/config";
 import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./courses/routes.js";
@@ -7,11 +8,11 @@ import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
 
 const app = express()
-app.use(cors());
+app.use(cors({origin: process.env.FRONTEND-URL}));
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
 Lab5(app);
 Hello(app)
-app.listen(4000)
+app.listen(process.env.PORT || 4000)
