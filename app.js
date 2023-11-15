ef8559a9fc8b6707f8b69f388f9b6e49d4df1547
Chaia <chaia.yodaiken@gmail.com>
2023-11-14 20:32:46 -0500

    init pass of assignment

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 import express from 'express';
+import cors from "cors";
 import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./courses/routes.js";
-import cors from "cors";
+import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from "./assignments/routes.js";
 
 const app = express()
 app.use(cors());
 app.use(express.json());
+ModuleRoutes(app);
 CourseRoutes(app);
+AssignmentRoutes(app);
 Lab5(app);
 Hello(app)
 app.listen(4000)
